e32f28d1c5dc3c1a005ab8a32ed63f71c0d09d13
Alex Smith <ags@fastmail.fm>
2018-08-27 23:35:22 +1000

    wip: broadsheet

---
+++ broadsheet/broadsheet.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Query struct {
+	City       string
+	Suburb     string
+	Categories []string
+	PageNumber int
+}
+
+type Client struct {
+	http *http.Client
+}
+
+func NewClient() *Client {
+	return &Client{
+		http: &http.Client{
+			Timeout: 30 * time.Second,
+		},
+	}
+}
+
+type Response struct {
+	Count   int      `json:"count"`
+	Results []Result `json:"results"`
+}
+
+type Result struct {
+	URL      string `json:"url"`
+	Suburb   string `json:"suburb"`
+	Title    string `json:"title"`
+	Category string `json:"category"`
+}
+
+type searchOption struct {
+	Categories []string `json:"categories"`
+}
+
+const (
+	apiURL = "https://www.broadsheet.com.au/api"
+)
+
+func (c *Client) Search(q Query) (Response, error) {
+	opt, err := json.Marshal(searchOption{
+		Categories: q.Categories,
+	})
+	if err != nil {
+		return Response{}, err
+	}
+
+	// TODO URL encoding suburb?
+	url := fmt.Sprintf(
+		"%s/%s/search/%s?page=%d&o=%s",
+		apiURL, q.City, q.Suburb, q.PageNumber, opt,
+	)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return Response{}, err
+	}
+
+	req.Header.Add("Accept", "application/json")
+
+	res, err := c.http.Get(url)
+	if err != nil {
+		return Response{}, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return Response{}, fmt.Errorf("expected 200, got %d", res.StatusCode)
+	}
+
+	var r Response
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		return Response{}, err
+	}
+	return r, nil
+}
---
+++ broadsheet/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"sync"
+)
+
+func main() {
+	scraper := &scraper{}
+	var (
+		city     = "sydney"
+		category = "restaurant"
+		suburbs  = []string{}
+	)
+	for _, s := range sydneySuburbs {
+		suburbs = append(suburbs, strings.ToLower(s))
+	}
+	//suburbs = []string{"prahran", "south yarra", "collingwood"}
+
+	results, err := scraper.scrape(city, category, suburbs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csv := csv.NewWriter(os.Stdout)
+	for _, r := range results {
+		err := csv.Write([]string{
+			city,
+			r.Suburb,
+			r.Category,
+			r.Title,
+			r.URL,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	csv.Flush()
+}
+
+type scraper struct{}
+
+func (s *scraper) scrape(city, category string, suburbs []string) ([]Result, error) {
+	sch := make(chan string, 10)
+	outch := make(chan output, 10)
+	var wg sync.WaitGroup
+
+	for w := 1; w <= 10; w++ {
+		wg.Add(1)
+
+		go func(id int) {
+			client := NewClient()
+			for s := range sch {
+				fmt.Fprintf(os.Stderr, "id=%d suburb=%s state=%s\n", id, s, "start")
+				r, err := client.Search(Query{
+					City:       city,
+					Suburb:     s,
+					Categories: []string{category},
+					PageNumber: 1,
+				})
+				outch <- output{suburb: s, response: r, err: err}
+				fmt.Fprintf(os.Stderr, "id=%d suburb=%s state=%s err=%v\n", id, s, "done", err)
+			}
+			wg.Done()
+		}(w)
+	}
+
+	go func() {
+		for _, s := range suburbs {
+			sch <- s
+		}
+		close(sch)
+
+		wg.Wait()
+		close(outch)
+	}()
+
+	results := []Result{}
+	for m := range outch {
+		if m.err != nil {
+			return []Result{}, m.err
+		}
+		for _, r := range m.response.Results {
+			results = append(results, r)
+		}
+	}
+
+	wg.Wait()
+
+	return results, nil
+}
+
+type output struct {
+	suburb   string
+	response Response
+	err      error
+}
